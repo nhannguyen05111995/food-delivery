95e78ad17b8699b91f798e7fcfa97da8391da229
nhannguyen05111995 <91246527+nhannguyen05111995@users.noreply.github.com>
2025-03-07 15:16:32 +0200

    add icon to cart buttons

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -18,7 +18,21 @@ export default function Cart() {
       <p>Here are the items in your cart.</p>
       <ul>
         {cart.items.map((item) => (
-          <li key={item._id}>{item.name} Quantity: {item.quantity} <button onClick={() => handleAdd(item)}>+</button><button onClick={() => handleRemove(item)}>-</button></li>
+          <li key={item._id}>{item.name} Quantity: {item.quantity}
+            <div className="ml-3 inline-flex rounded-md shadow-xs" role="group">
+              <button onClick={() => handleAdd(item)} type="button" className="text-xs inline-flex items-center px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+                <svg className="w-[15px] h-[15px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
+                </svg>
+
+              </button>
+              <button onClick={() => handleRemove(item)} type="button" className="text-xs px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+                <svg className="w-[15px] h-[15px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14" />
+                </svg>
+              </button>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
--- app/meals/add-meal/page.tsx
+++ app/meals/add-meal/page.tsx
@@ -51,10 +51,12 @@ export default function Page() {
 
     }
     return (
-        <div>
-            <h1>Meals</h1>
-            <div className="w-full max-w-xs">
-                <form className="pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+        <div className='mt-5'>
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">We invest in the world’s potential</h1>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            
+            <div className="w-full ">
+                <form className="pt-6 pb-8 mb-4 max-w-xs mx-auto" onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <Input props={{ name: 'name', 'type': 'text', 'errors': errors }} />
                     </div>
@@ -74,7 +76,7 @@ export default function Page() {
 
                     </div>
                     <div className="flex items-center justify-between form-action">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" disabled={loading}>
+                        <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" disabled={loading}>
                             Add meal
                         </button>
                     </div>
